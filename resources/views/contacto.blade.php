aecffe8d254a7476c8acad0d4677ce6dccd591b9
ecreyes <ecreyes@alumnos.inf.utfsm.cl>
2017-11-13 21:21:43 -0300

    -Se crearon las vistas iniciales del sitio. -Se importo Bootstrap al layout.

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('contenido')
+<div class="container">
+	<h1>Contactanos</h1>	
+</div>
+@stop
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('contenido')
+<div class="container">
+</div>
+
+<div class="container">
+	<h1>Bienvenido a la plataforma</h1>
+</div>
+@stop
